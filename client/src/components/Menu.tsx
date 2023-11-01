0524d988c182b3f9733b9abb1c4256ccde81b5f8
KimJaewon3 <poilkj1po@gmail.com>
2023-11-01 17:31:01 +0900

    feat: 회원가입 폼 추가

---
+++ client/src/components/Menu.tsx
@@ -0,0 +1,27 @@
+import { styled } from "styled-components";
+import { NavLink } from "react-router-dom";
+
+type Props = {
+  pathList: string[];
+};
+
+export default function Menu({ pathList }: Props) {
+  function getPageName(path: string) {
+    if (path === "/") return "home";
+    return path.split("/")[1];
+  }
+
+  return (
+    <MenuContainer>
+      <ul>
+        {pathList.map((path) => (
+          <li key={path}>
+            <NavLink to={path}>{getPageName(path)}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </MenuContainer>
+  );
+}
+
+const MenuContainer = styled.nav``;
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <div>im home</div>
+    </HomeContainer>
+  );
+}
+
+const HomeContainer = styled.div``;
