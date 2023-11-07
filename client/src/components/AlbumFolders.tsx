1edd93abf3d913083a8dc25b6b976dc339da8ea3
KimJaewon3 <poilkj1po@gmail.com>
2023-11-07 16:19:02 +0900

    feat: 앨범 폴더 메뉴 추가

--- client/src/components/AlbumFolders.tsx
+++ client/src/components/AlbumFolders.tsx
@@ -1,71 +1,114 @@
-import { useRecoilState } from "recoil";
-import { albumState } from "../store/album";
-import { useState } from "react";
 import styled from "styled-components";
-import { postFolder } from "../api/album";
+import { BiSolidPhotoAlbum } from "react-icons/bi";
+import type { ReactNode } from "react";
+import {
+  BsFillCameraReelsFill,
+  BsFillBrightnessHighFill,
+  BsFillAirplaneFill,
+  BsFillTelephoneFill,
+  BsFillTreeFill,
+  BsCloudFill,
+} from "react-icons/bs";
 
-export default function AlbumFolders() {
-  const [album, setAlbum] = useRecoilState(albumState);
-  const [folderName, setFolderName] = useState("");
+function albumIconGenerator(name: string): ReactNode {
+  const icons: {
+    [key: string]: ReactNode;
+  } = {
+    albums: <BiSolidPhotoAlbum />,
+    camera: <BsFillCameraReelsFill />,
+    bright: <BsFillBrightnessHighFill />,
+    airplane: <BsFillAirplaneFill />,
+    telephone: <BsFillTelephoneFill />,
+    tree: <BsFillTreeFill />,
+    cloud: <BsCloudFill />,
+  };
 
-  async function addFolder() {
-    if (folderName === "") return;
-    // const addedFolder = (await postFolder(folderName)).data;
-    const addedFolder = {
-      id: album.folders.length,
-      name: folderName,
-    };
+  return icons[name];
+}
 
-    setAlbum({
-      ...album,
-      folders: [...album.folders, addedFolder],
-    });
-    setFolderName("");
-  }
+type Props = {
+  toggleFolderMenu: () => void;
+};
 
-  async function handleEnter(e: React.KeyboardEvent<HTMLInputElement>) {
-    if (e.key === "Enter") {
-      await addFolder();
-    }
-  }
+export default function AlbumFolders({ toggleFolderMenu }: Props) {
+  const dummyAlbumList = [
+    {
+      id: 1,
+      name: "album1",
+      icon: "camera",
+    },
+    {
+      id: 2,
+      name: "album2",
+      icon: "bright",
+    },
+    {
+      id: 3,
+      name: "album3",
+      icon: "airplane",
+    },
+    {
+      id: 4,
+      name: "album4",
+      icon: "telephone",
+    },
+    {
+      id: 5,
+      name: "album5",
+      icon: "tree",
+    },
+    {
+      id: 6,
+      name: "album6",
+      icon: "cloud",
+    },
+  ];
 
-  async function deleteFolder(targetId: number) {
-    // await deleteFolder(targetId);
-
-    setAlbum({
-      ...album,
-      folders: album.folders.filter(({ id }) => id !== targetId),
-    });
+  function filteringPhotos(albumName: string) {
+    // 해당 앨범의 포토를 보여준다
+    console.log("filtering photos", albumName);
   }
 
   return (
     <AlbumFoldersContainer>
-      <div>
-        <input
-          type="text"
-          placeholder="folder name here..."
-          value={folderName}
-          onKeyDown={(e) => handleEnter(e)}
-          onChange={(e) => setFolderName(e.target.value)}
-        />
-        <button onClick={addFolder}>+</button>
-      </div>
-
-      <ul>
-        <li>all</li>
-        {album.folders.map((folder) => (
-          <li key={folder.id} className="folder-li">
-            <div>{folder.name}</div>
-            <button onClick={() => deleteFolder(folder.id)}>x</button>
-          </li>
+      <ul className="album-menu">
+        <AlbumLi name="Albums" onClick={toggleFolderMenu} icon={albumIconGenerator("albums")} />
+        <AlbumLi name="All" onClick={() => filteringPhotos("all")} />
+        {dummyAlbumList.map(({ icon, name, id }) => (
+          <AlbumLi
+            name={name}
+            key={id}
+            icon={albumIconGenerator(icon)}
+            onClick={() => filteringPhotos(name)}
+          />
         ))}
       </ul>
     </AlbumFoldersContainer>
   );
 }
 
+type LiProps = { name: string; onClick?: () => void; icon?: ReactNode };
+
+const AlbumLi = ({ name, onClick, icon }: LiProps) => {
+  return (
+    <li onClick={onClick}>
+      {icon}
+      <div>{name}</div>
+    </li>
+  );
+};
+
 const AlbumFoldersContainer = styled.div`
-  .folder-li {
-    display: flex;
+  min-height: 100vh;
+  background: linear-gradient(to bottom right, #e2a763, #8b4414);
+  .album-menu > li {
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: 60px 1fr;
+    align-items: center;
+    padding: 1rem;
+    border-bottom: 2px solid white;
+  }
+  .album-menu > li:first-child {
   }
 `;
