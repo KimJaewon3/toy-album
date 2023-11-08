ba989ea5dd94fd0d42dc404c8c446e05759c730f
KimJaewon3 <poilkj1po@gmail.com>
2023-11-08 16:44:30 +0900

    feat: 포토 자세히 보기 모달 추가

--- client/src/components/DetailPhoto.tsx
+++ client/src/components/DetailPhoto.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { albumState, currentPhotoInfo } from "../store/album";
+import { AiOutlineInfoCircle, AiOutlineZoomIn } from "react-icons/ai";
 
 export default function DetailPhoto() {
   const photoInfo = useRecoilValue(currentPhotoInfo);
@@ -9,18 +10,42 @@ export default function DetailPhoto() {
   useEffect(() => {
     console.log(photoInfo);
   }, []);
+
+  if (!photoInfo) return null;
+
   return (
     <DetailPhotoContainer>
-      <div>{photoInfo?.id}</div>
-      <div>{photoInfo?.src}</div>
-      <div>{photoInfo?.name}</div>
-      <div>{photoInfo?.description}</div>
+      <div className="img-btns">
+        <AiOutlineZoomIn size="20px" color="white" />
+        <AiOutlineInfoCircle size="20px" color="white" />
+      </div>
+
+      <div className="img-wrap">
+        <img src={photoInfo?.src} />
+      </div>
     </DetailPhotoContainer>
   );
 }
 
 const DetailPhotoContainer = styled.div`
-  background-color: white;
-  width: 100px;
-  height: 100px;
+  .img-wrap {
+    background-color: white;
+    padding: 5px;
+
+    > img {
+      min-width: 300px;
+      min-height: 300px;
+      max-width: 700px;
+      max-height: 700px;
+    }
+  }
+
+  .img-btns {
+    display: flex;
+    justify-content: end;
+    padding-bottom: 5px;
+    > * {
+      margin-left: 10px;
+    }
+  }
 `;
--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -28,7 +28,7 @@ const ModalContainer = styled.div`
   left: 0;
   width: 100vw;
   height: 100vh;
-  background-color: rgba(0, 0, 0, 0.137);
+  background-color: rgba(0, 0, 0, 0.7);
   display: flex;
   justify-content: center;
   align-items: center;
