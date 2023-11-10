d7f020dc196ec5ed73a7c4efd392eca00a11c1f0
KimJaewon3 <poilkj1po@gmail.com>
2023-11-10 15:59:21 +0900

    feat: 사진 상세정보 및 에딧모드 추가

--- client/src/components/DetailPhoto.tsx
+++ client/src/components/DetailPhoto.tsx
@@ -1,27 +1,32 @@
 import styled from "styled-components";
-import { useEffect } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { albumState, currentPhotoInfo } from "../store/album";
+import { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { albumState, currentPhoto } from "../store/album";
 import { AiOutlineInfoCircle, AiOutlineZoomIn } from "react-icons/ai";
+import { Photo } from "../api/album";
 
 export default function DetailPhoto() {
-  const photoInfo = useRecoilValue(currentPhotoInfo);
+  const photoInfo = useRecoilValue(currentPhoto);
+  const [isInfoOpen, setIsInfoOpen] = useState(false);
 
-  useEffect(() => {
-    console.log(photoInfo);
-  }, []);
-
-  if (!photoInfo) return null;
+  function toggleInfo() {
+    setIsInfoOpen(!isInfoOpen);
+  }
 
   return (
     <DetailPhotoContainer>
       <div className="img-btns">
-        <AiOutlineZoomIn size="20px" color="white" />
-        <AiOutlineInfoCircle size="20px" color="white" />
+        <div>
+          <AiOutlineZoomIn size="20px" color="white" />
+        </div>
+        <div onClick={toggleInfo}>
+          <AiOutlineInfoCircle size="20px" color="white" />
+        </div>
       </div>
 
       <div className="img-wrap">
         <img src={photoInfo?.src} />
+        {isInfoOpen && <PhotoInfo info={photoInfo} />}
       </div>
     </DetailPhotoContainer>
   );
@@ -31,6 +36,7 @@ const DetailPhotoContainer = styled.div`
   .img-wrap {
     background-color: white;
     padding: 5px;
+    position: relative;
 
     > img {
       min-width: 300px;
@@ -47,5 +53,91 @@ const DetailPhotoContainer = styled.div`
     > * {
       margin-left: 10px;
     }
+  }
+`;
+
+function PhotoInfo({ info: { date, description, name, id } }: { info: Photo }) {
+  const setAlbum = useSetRecoilState(albumState);
+  const [editMode, setEditMode] = useState({
+    name: false,
+    date: false,
+    description: false,
+  });
+
+  function toggleEditMode(editTarget: keyof typeof editMode) {
+    setEditMode({
+      ...editMode,
+      [editTarget]: !editMode[editTarget],
+    });
+  }
+
+  function handleSubmit(
+    e: React.KeyboardEvent<HTMLInputElement>,
+    editTarget: keyof typeof editMode
+  ) {
+    if (e.key !== "Enter") return;
+    setAlbum((prev) => {
+      return {
+        ...prev,
+        photos: prev.photos.map((item) => {
+          if (item.id === id) {
+            item = {
+              ...item,
+              [editTarget]: e.currentTarget.value,
+            };
+          }
+          return item;
+        }),
+      };
+    });
+    toggleEditMode(editTarget);
+  }
+
+  return (
+    <PhotoInfoContainer>
+      <div className="photo-name">
+        {editMode.name ? (
+          <input defaultValue={name} onKeyDown={(e) => handleSubmit(e, "name")} />
+        ) : (
+          <div onClick={() => toggleEditMode("name")}>{name}</div>
+        )}
+      </div>
+      <div className="photo-date">
+        {editMode.date ? (
+          <input defaultValue={date} onKeyDown={(e) => handleSubmit(e, "date")} />
+        ) : (
+          <div onClick={() => toggleEditMode("date")}>{date}</div>
+        )}
+      </div>
+      <div className="photo-description">
+        {editMode.description ? (
+          <input defaultValue={description} onKeyDown={(e) => handleSubmit(e, "description")} />
+        ) : (
+          <div onClick={() => toggleEditMode("description")}>{description}</div>
+        )}
+      </div>
+    </PhotoInfoContainer>
+  );
+}
+
+const PhotoInfoContainer = styled.div`
+  > div {
+    position: absolute;
+    background-color: white;
+  }
+  .photo-name {
+    top: 0;
+    left: 0;
+  }
+  .photo-date {
+    bottom: 0;
+    right: 0;
+  }
+  .photo-description {
+    top: 0;
+    right: -200px;
+    background-color: white;
+    width: 200px;
+    height: 100%;
   }
 `;
--- client/src/store/album.ts
+++ client/src/store/album.ts
@@ -9,8 +9,8 @@ import p5 from "../assets/images/p5.jpg";
 
 type AlbumState = {
   folders: { id: number; name: string }[]; // 폴더 이름만
-  photos: string[]; // 전체 사진
-  currentPhoto: Photo | null;
+  photos: Photo[]; // 전체 사진
+  currentPhotoId: number;
 };
 
 const albumState = atom<AlbumState>({
@@ -18,7 +18,7 @@ const albumState = atom<AlbumState>({
   default: {
     folders: [],
     photos: [],
-    currentPhoto: null,
+    currentPhotoId: 0,
   },
 });
 
@@ -29,35 +29,43 @@ const albumFolders = selector({
   },
 });
 
-const currentPhotoInfo = selector({
-  key: "currentPhotoInfo",
+const albumPhotos = selector({
+  key: "albumPhotos",
   get: ({ get }) => {
-    return get(albumState).currentPhoto;
+    return get(albumState).photos;
+  },
+});
+
+const currentPhoto = selector({
+  key: "currentPhoto",
+  get: ({ get }) => {
+    const { photos, currentPhotoId } = get(albumState);
+    return photos.filter((p) => p.id === currentPhotoId)[0];
   },
 });
 
 // /album/picture?folderId={없으면 all}&page=1&sortBy={asc/desc}&name=&description=
 export const dummyPhotos: Photo[] = [
-  { id: 1, description: "im picture 1", name: "P1", src: p1 },
-  { id: 2, description: "im picture 2", name: "P2", src: p2 },
-  { id: 3, description: "im picture 3", name: "P3", src: p3 },
-  { id: 4, description: "im picture 4", name: "P4", src: p4 },
-  { id: 5, description: "im picture 5", name: "P5", src: p5 },
-  { id: 6, description: "im picture 6", name: "P6", src: p1 },
-  { id: 7, description: "im picture 7", name: "P7", src: p2 },
-  { id: 8, description: "im picture 8", name: "P8", src: p3 },
-  { id: 9, description: "im picture 9", name: "P9", src: p4 },
-  { id: 10, description: "im picture 10", name: "P10", src: p5 },
-  { id: 11, description: "im picture 11", name: "P11", src: p1 },
-  { id: 12, description: "im picture 12", name: "P12", src: p2 },
-  { id: 13, description: "im picture 13", name: "P13", src: p3 },
-  { id: 14, description: "im picture 14", name: "P14", src: p4 },
-  { id: 15, description: "im picture 15", name: "P15", src: p5 },
-  { id: 16, description: "im picture 16", name: "P16", src: p1 },
-  { id: 17, description: "im picture 17", name: "P17", src: p2 },
-  { id: 18, description: "im picture 18", name: "P18", src: p3 },
-  { id: 19, description: "im picture 19", name: "P19", src: p4 },
-  { id: 20, description: "im picture 20", name: "P20", src: p5 },
+  { id: 1, description: "im picture 1", name: "P1", src: p1, date: "2023-03-01" },
+  { id: 2, description: "im picture 2", name: "P2", src: p2, date: "2023-03-01" },
+  { id: 3, description: "im picture 3", name: "P3", src: p3, date: "2023-03-01" },
+  { id: 4, description: "im picture 4", name: "P4", src: p4, date: "2023-03-01" },
+  { id: 5, description: "im picture 5", name: "P5", src: p5, date: "2023-03-01" },
+  { id: 6, description: "im picture 6", name: "P6", src: p1, date: "2023-03-01" },
+  { id: 7, description: "im picture 7", name: "P7", src: p2, date: "2023-03-01" },
+  { id: 8, description: "im picture 8", name: "P8", src: p3, date: "2023-03-01" },
+  { id: 9, description: "im picture 9", name: "P9", src: p4, date: "2023-03-01" },
+  { id: 10, description: "im picture 10", name: "P10", src: p5, date: "2023-03-01" },
+  { id: 11, description: "im picture 11", name: "P11", src: p1, date: "2023-03-01" },
+  { id: 12, description: "im picture 12", name: "P12", src: p2, date: "2023-03-01" },
+  { id: 13, description: "im picture 13", name: "P13", src: p3, date: "2023-03-01" },
+  { id: 14, description: "im picture 14", name: "P14", src: p4, date: "2023-03-01" },
+  { id: 15, description: "im picture 15", name: "P15", src: p5, date: "2023-03-01" },
+  { id: 16, description: "im picture 16", name: "P16", src: p1, date: "2023-03-01" },
+  { id: 17, description: "im picture 17", name: "P17", src: p2, date: "2023-03-01" },
+  { id: 18, description: "im picture 18", name: "P18", src: p3, date: "2023-03-01" },
+  { id: 19, description: "im picture 19", name: "P19", src: p4, date: "2023-03-01" },
+  { id: 20, description: "im picture 20", name: "P20", src: p5, date: "2023-03-01" },
 ];
 
-export { albumState, albumFolders, currentPhotoInfo };
+export { albumState, albumFolders, currentPhoto, albumPhotos };
